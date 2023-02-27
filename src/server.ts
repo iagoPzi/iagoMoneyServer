e0f2d984353b2f20ad36f4fbdbee15842afd4402
Iago Pzivitovski <iagopzivitovski@gmail.com>
2023-02-27 16:18:53 -0300

    add PORT env

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,13 @@
-
 import cors from "cors";
 import express from "express";
 import { transactionRoutes } from "./routes/transactions/transactions.routes";
-
-
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use(transactionRoutes)
+app.use(transactionRoutes);
 
-
-app.listen(3333, () => {
+app.listen(process.env.PORT || 3333, () => {
   console.log("server running");
 });
